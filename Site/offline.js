05db67e727e1c9bb4b9b26d6b210ad9929f40734
Poishiche <damien.belghit@yahoo.fr>
2021-01-30 12:12:08 +0100

    Importation du site

---
+++ Site/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1612005038,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/planet-sheet0.png",
+		"images/planet-sheet1.png",
+		"images/background-sheet0.png",
+		"images/bluezone.png",
+		"images/back-sheet0.png",
+		"images/backback-sheet0.png",
+		"images/road-sheet0.png",
+		"images/road-sheet1.png",
+		"images/road-sheet2.png",
+		"images/button-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
